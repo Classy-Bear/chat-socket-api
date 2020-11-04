a9e1d22c33bff4adff9d0fa87a0f52d6830b8b4a
ClassyBear <davidfco1702@protonmail.com>
2020-11-04 19:18:21 -0400

    socket.io starting point

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import * as e from './modules/socket_events';
-
 const express = require('express');
 const Socket = require('socket.io');
+const {
+  onSave, askForMessage, connected, ack, disconnected,
+} = require('./modules/socket_events');
 
 const PORT = 4000;
 const app = express();
 const server = app.listen(PORT);
 const io = new Socket(server);
 io.on('connection', (socket) => {
-  socket.on('save', (data) => e.onSave(data, io));
-  socket.on('ask for message', (data) => e.askForMessage(data, io));
-  socket.on('ack', e.ack);
-  socket.on('connected', (data) => e.connected(data, socket));
-  socket.on('disconnected', (data) => e.disconnected(data, socket));
+  socket.on('save', (data) => onSave(data, io));
+  socket.on('ask for message', (data) => askForMessage(data, io));
+  socket.on('ack', ack);
+  socket.on('connected', (data) => connected(data, socket));
+  socket.on('disconnected', (data) => disconnected(data, socket));
 });
